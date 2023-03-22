9f1d18ab283b7fa36de443034c1d9e7134f10d24
frankcl <frank.lichen@gmail.com>
2023-03-22 15:37:35 +0800

    add JobServiceImpl and URLServiceImpl for OTS

--- darwin-common/src/main/java/xin/manong/darwin/common/model/RangeValue.java
+++ darwin-common/src/main/java/xin/manong/darwin/common/model/RangeValue.java
@@ -1,5 +1,7 @@
 package xin.manong.darwin.common.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
@@ -11,10 +13,15 @@ import lombok.experimental.Accessors;
  */
 @Setter
 @Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RangeValue<T extends Number> {
 
+    @JsonProperty("include_lower")
     public boolean includeLower = false;
+    @JsonProperty("include_upper")
     public boolean includeUpper = false;
+    @JsonProperty("start")
     public T start;
+    @JsonProperty("end")
     public T end;
 }
