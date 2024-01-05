6cc06e6967468d2e953b8f8563153e867278f5d0
frankcl <frank.lichen@gmail.com>
2024-01-05 18:28:57 +0800

    support to sweep Log4J logger for HTMLParser, avoid OOM

--- darwin-parser/src/main/java/xin/manong/darwin/parser/sdk/HTMLParser.java
+++ darwin-parser/src/main/java/xin/manong/darwin/parser/sdk/HTMLParser.java
@@ -1,10 +1,15 @@
 package xin.manong.darwin.parser.sdk;
 
 import org.apache.log4j.*;
+import org.apache.log4j.spi.LoggerRepository;
+import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import xin.manong.darwin.parser.log.MemoryWriterAppender;
+import xin.manong.weapon.base.util.ReflectUtil;
 
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -18,6 +23,7 @@ public abstract class HTMLParser {
 
     private static final String LOG_LAYOUT_PATTERN = "%-d{yyyy-MM-dd HH:mm:ss,SSS}-%r [%p] [%t] [%l] - %m%n";
 
+    private Logger selfLogger = LoggerFactory.getLogger(HTMLParser.class);
     protected Logger logger;
 
     /**
@@ -42,7 +48,8 @@ public abstract class HTMLParser {
             return response;
         } finally {
             if (appender != null) appender.close();
-            logger = null;
+            sweepLoggerFactory(name);
+            sweepLogManager(name);
         }
     }
 
@@ -53,4 +60,37 @@ public abstract class HTMLParser {
      * @return 解析响应
      */
     public abstract ParseResponse parse(ParseRequest request);
+
+    /**
+     * 清理SLF4J logger
+     *
+     * @param name logger名称
+     */
+    private void sweepLoggerFactory(String name) {
+        try {
+            ILoggerFactory factory = LoggerFactory.getILoggerFactory();
+            Map<String, Logger> logMap = (Map<String, Logger>) ReflectUtil.getFieldValue(factory, "loggerMap");
+            if (logMap == null) return;
+            logMap.remove(name);
+        } catch (Exception e) {
+            selfLogger.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * 清理Log4J logger
+     *
+     * @param name logger名称
+     */
+    private void sweepLogManager(String name) {
+        try {
+            LoggerRepository loggerRepository = LogManager.getLoggerRepository();
+            Hashtable<String, org.apache.log4j.Logger> logTable = (Hashtable<String, org.apache.log4j.Logger>)
+                    ReflectUtil.getFieldValue(loggerRepository, "ht");
+            if (logTable == null) return;
+            logTable.remove(name);
+        } catch (Exception e) {
+            selfLogger.error(e.getMessage(), e);
+        }
+    }
 }
