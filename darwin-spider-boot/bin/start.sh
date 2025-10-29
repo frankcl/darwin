b2a01f02f5da86e9169e113edb6e6d8b97e78300
frankcl <frank.lichen@gmail.com>
2025-10-29 15:11:45 +0800

    add MessagePusher impl

--- darwin-spider-boot/bin/start.sh
+++ darwin-spider-boot/bin/start.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 mkdir log
 
 exec java -Xlog:gc=info:file=./log/gc.log:uptimemillis,pid:filecount=5,filesize=1m -XX:+UseG1GC \
-    -Xms300m -Xmx1000m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./log/heap.dump \
+    -Xms100m -Xmx500m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./log/heap.dump \
     -cp "../lib/*" -Dlog4j.configuration="file:../conf/log4j.properties" \
     -Djava.security.manager -Djava.security.policy="file:../conf/darwin.policy" \
     -Dpolyglot.engine.WarnInterpreterOnly=false \
