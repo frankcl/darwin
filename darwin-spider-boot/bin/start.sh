bc0a8896e0d608631e2fb8d1c5c6c0150ca87b0a
frankcl <frank.lichen@gmail.com>
2025-12-01 14:02:13 +0800

    add process management for start.sh

--- darwin-spider-boot/bin/start.sh
+++ darwin-spider-boot/bin/start.sh
@@ -4,9 +4,19 @@ cd `dirname $0`
 
 mkdir log
 
+pid_file="../pid"
+
+if [ -f ${pid_file} ]; then
+  prev_pid=`cat ${pid_file}`
+  echo "Kill process ${prev_pid}"
+  kill -9 ${prev_pid}
+fi
+
 exec java -Xlog:gc=info:file=./log/gc.log:uptimemillis,pid:filecount=5,filesize=1m -XX:+UseG1GC \
     -Xms300m -Xmx1000m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./log/heap.dump \
     -cp "../lib/*" -Dlog4j.configuration="file:../conf/log4j.properties" \
     -Djava.security.manager -Djava.security.policy="file:../conf/darwin.policy" \
     -Dpolyglot.engine.WarnInterpreterOnly=false \
     xin.manong.darwin.spider.Application > stderr.out 2>&1 & disown
+
+echo $! > ${pid_file}
